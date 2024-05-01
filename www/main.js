d899f4bde77ae3876c93f3acd7796d750d6aa9b5
Astha <asthadwivedi2003@gmail.com>
2024-05-01 19:43:24 +0530

    AVAHOOD design done

---
+++ www/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+        $(document).ready(function () {
+            $('.text').texillate({
+                loop: true,
+                sync: true,
+                in: {
+                    effect: "bouncIn",
+                },
+                out:{
+                    effect: "bounceout",
+                },
+            });
+        });
+    
+    });
